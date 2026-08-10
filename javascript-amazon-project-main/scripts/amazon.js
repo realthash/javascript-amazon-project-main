8a6715f975e13c053f5eee4d78ef134d9846df98
realthash <thashmikarathnayake11@gmail.com>
2026-08-10 23:07:16 +0530

    refactor: extract currency formatting to shared utility function
    Replace inline price calculation logic with formatCurrency utility in amazon.js and checkout.js. Create utils/price.js module to centralize currency formatting for better code reuse and consistency. Also fix trailing whitespace and add unique delivery option radio names.

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addtoCart } from '../data/cart.js'
 import { products } from '../data/products.js'
+import { formatCurrency } from '../scripts/utils/price.js'
 let html = ''
 
 products.forEach((product) => {
@@ -22,7 +23,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-          $${(product.priceCents / 100).toFixed(2)}
+          $${formatCurrency(product.priceCents)}
         </div>
 
         <div class="product-quantity-container">
